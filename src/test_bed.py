58f70e856c44fbcf8058024697c63ace206109cb
Robert Cooper <Robert.Cooper@lch.com>
2018-05-02 16:13:40 +0100

    Building Summary

--- src/test_bed.py
+++ src/test_bed.py
@@ -11,11 +11,7 @@ replace_tuples = [
     ('facilities', [('Total Facility A Commitments', 'total_fac_a')])
 ]
 
-# print(lfas.get_value('facilities'))
-print('Total Facilities A is {} and B is {}'.format(lfas.get_value('total_fac_a'),lfas.get_value('total_fac_b')))
+print('Total Facilities A is {} and B is {}'.format(lfas.get_value('total_fac_a'), lfas.get_value('total_fac_b')))
 
-# for section, rep_list in replace_tuples:
-#     txt = lfas.get_value(section)
-#     for name, var_name in rep_list:
-#         txt = txt.replace(name, lfas.get_value(var_name))
-#     print(txt)
+
+
